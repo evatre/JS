0f16b95bcb765e61f6013d28c60c2563bf013fe1
Eva on Mac <treija.eva@gmail.com>
2022-08-16 12:48:49 +0300

    trying to make things disappear but not succeeding

---
+++ 2022.08.11.table/test.js
@@ -0,0 +1,12 @@
+function filterWhite() {
+  // var white = document.getElementById("white");
+  var yellow = document.getElementById("yellow");
+  // var orange = document.getElementById("orange");
+  // var red = document.getElementById("red");
+  document.getElementById("yellow").hidden = "true";
+  // if (yellow.style.display === "none") {
+  //   yellow.style.display = "block";
+  // } else {
+  //   yellow.style.display = "none";
+  // }
+}
